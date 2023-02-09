fb7d51ea4c293c9db20091878c6324137f36e093
Rayyan <rayyanaslam16@gmail.com>
2023-02-09 18:48:45 +0530

    fixed bug of decimal point on empty state of current operand

--- src/App.js
+++ src/App.js
@@ -19,29 +19,40 @@ function reducer(state, { type, payload }) {
           ...state,
           currentOperand: payload.digit,
           overwrite: false,
-        }
+        };
       }
+
+      if(state.currentOperand == null && payload.digit === "."){
+        return{
+
+          ...state,
+          currentOperand: payload.digit  
+        };
+      }
+
       if (payload.digit === "0" && state.currentOperand === "0") {
-        return state
+        return state;
       }
       if (payload.digit === "." && state.currentOperand.includes(".")) {
-        return state
+        console.log("double dot");
+        return state;
       }
+      
 
       return {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
-      }
+      };
     case ACTIONS.CHOOSE_OPERATION:
       if (state.currentOperand == null && state.previousOperand == null) {
-        return state
+        return state;
       }
 
       if (state.currentOperand == null) {
         return {
           ...state,
           operation: payload.operation,
-        }
+        };
       }
 
       if (state.previousOperand == null) {
@@ -50,7 +61,7 @@ function reducer(state, { type, payload }) {
           operation: payload.operation,
           previousOperand: state.currentOperand,
           currentOperand: null,
-        }
+        };
       }
 
       return {
@@ -58,33 +69,34 @@ function reducer(state, { type, payload }) {
         previousOperand: evaluate(state),
         operation: payload.operation,
         currentOperand: null,
-      }
+      };
     case ACTIONS.CLEAR:
-      return {}
+      return {};
     case ACTIONS.DELETE_DIGIT:
       if (state.overwrite) {
         return {
           ...state,
           overwrite: false,
           currentOperand: null,
-        }
+        };
       }
-      if (state.currentOperand == null) return state
+      if (state.currentOperand == null) 
+        return state;
       if (state.currentOperand.length === 1) {
-        return { ...state, currentOperand: null }
+        return { ...state, currentOperand: null };
       }
 
       return {
         ...state,
         currentOperand: state.currentOperand.slice(0, -1),
-      }
+      };
     case ACTIONS.EVALUATE:
       if (
         state.operation == null ||
         state.currentOperand == null ||
         state.previousOperand == null
       ) {
-        return state
+        return state;
       }
 
       return {
@@ -93,45 +105,52 @@ function reducer(state, { type, payload }) {
         previousOperand: null,
         operation: null,
         currentOperand: evaluate(state),
-      }
+      };
   }
 }
 
 function evaluate({ currentOperand, previousOperand, operation }) {
+
   const prev = parseFloat(previousOperand)
   const current = parseFloat(currentOperand)
-  if (isNaN(prev) || isNaN(current)) return ""
-  let computation = ""
+
+  if (isNaN(prev) || isNaN(current)) 
+    return "";
+
+  let computation = "";
   switch (operation) {
     case "+":
-      computation = prev + current
-      break
+      computation = prev + current;
+      break;
     case "-":
-      computation = prev - current
-      break
+      computation = prev - current;
+      break;
     case "*":
-      computation = prev * current
-      break
+      computation = prev * current;
+      break;
     case "÷":
-      computation = prev / current
-      break
+      computation = prev / current;
+      break;
   }
 
-  return computation.toString()
+  return computation.toString();
 }
 
 const INTEGER_FORMATTER = new Intl.NumberFormat("en-IN", {
   maximumFractionDigits: 0,
-})
+});
+
 function formatOperand(operand) {
-  if (operand == null) return
+  if (operand == null) 
+    return;
   const [integer, decimal] = operand.split(".")
-  if (decimal == null) return INTEGER_FORMATTER.format(integer)
-  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+  if (decimal == null) 
+    return INTEGER_FORMATTER.format(integer);
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
 }
 
 function App() {
-  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {});
 
   return (
     <div className="calculator-grid">
@@ -160,7 +179,7 @@ function App() {
       <DigitButton digit="0" dispatch={dispatch} />
       <button className="span-two" onClick={() => dispatch({ type: ACTIONS.EVALUATE })}> = </button>
     </div>
-  )
+  );
 }
 
 export default App
